e1748c0b91c26dcc7444c2fc1e3561b3d1e0de61
furiza31 <hugo.wendjaneh@gmail.com>
2025-12-03 16:08:36 +0100

    init

---
+++ Labyrinth/Items/MyInventory.cs
@@ -0,0 +1,14 @@
+﻿namespace Labyrinth.Items
+{
+    /// <summary>
+    /// Inventory class that exposes the item it contains.
+    /// </summary>
+    /// <param name="item">Optional initial item in the inventory.</param>
+    public class MyInventory(ICollectable? item = null) : Inventory(item)
+    {
+        /// <summary>
+        /// Items in the inventory.
+        /// </summary>
+        public IEnumerable<ICollectable> Items => _items;
+    }
+}
