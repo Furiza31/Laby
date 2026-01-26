545e0b94f45947867b6e9d5f2f247a73823ff31b
Furiza31 <hugo.wendjaneh@gmail.com>
2026-01-26 21:56:11 +0100

    init

--- Laby.Core/Items/ICollectable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Laby.Core.Items
-{
-    /// <summary>
-    /// A collectable item in the labyrinth.
-    /// </summary>
-    public interface ICollectable
-    {
-    }
-}
--- Laby.Core/Items/Key.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Laby.Core.Items
-{
-    /// <summary>
-    /// Collectable key item in the labyrinth.
-    /// </summary>
-    public class Key : ICollectable
-    {
-    }
-}
